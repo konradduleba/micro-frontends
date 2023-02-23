5b727da9d741b168c20504f35b46d9b52f627dbd
Konrad Dulęba <konrad.duleba@miquido.com>
2023-02-23 20:35:20 +0100

    feat(global): moved bimbows page to /bimbows-3-1, added welcome page

--- src/dulcon-root-config.ts
+++ src/dulcon-root-config.ts
@@ -1,18 +1,15 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import<LifeCycles>(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@dulcon/welcome",
+  app: () => System.import<LifeCycles>("@dulcon/welcome"),
+  activeWhen: ["/"],
+});
 
 registerApplication({
   name: "@dulcon/bimbows-3-1",
   app: () => System.import<LifeCycles>("@dulcon/bimbows-3-1"),
-  activeWhen: ["/"],
+  activeWhen: ["/bimbows-3-1"],
 });
 
 start({
